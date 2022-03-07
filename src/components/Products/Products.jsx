65d8b3153490a06c52d5bd8411472d588d1e118f
Vaishali2127 <vaishalimahour8@gmail.com>
2022-03-07 15:05:33 +0530

    Add all products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,24 +4,24 @@ import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Shoes",
-    description: "Running Shoes",
-    price: "$5",
-    image: "../../shoes.jpg",
-  },
-  {
-    id: 2,
-    name: "Macbook",
-    description: "Apple Macbook",
-    price: "$10",
-    image: "../../shoes.jpg",
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     name: "Shoes",
+//     description: "Running Shoes",
+//     price: "$5",
+//     image: "../../shoes.jpg",
+//   },
+//   {
+//     id: 2,
+//     name: "Macbook",
+//     description: "Apple Macbook",
+//     price: "$10",
+//     image: "../../shoes.jpg",
+//   },
+// ];
 
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
